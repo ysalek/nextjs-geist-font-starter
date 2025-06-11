6ee00114aee23f7c1944df2e0e478743d92e2158
ysalek <ysalek1@gmail.com>
2025-06-11 09:42:38 -0400

    Add .next/static/chunks/src_app_dashboard_agente_propiedades_crear_page_tsx_df2980a6._.js 1749649358351

---
+++ .next/static/chunks/src_app_dashboard_agente_propiedades_crear_page_tsx_df2980a6._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_aa08d714._.js",
+  "static/chunks/node_modules_8e9b0c99._.js"
+],
+    source: "dynamic"
+});
